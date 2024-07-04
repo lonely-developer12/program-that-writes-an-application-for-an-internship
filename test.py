a884f146d1f09832966481db1f8f08c75d6367e9
lonely-developer12 <emirbuzdic@gmail.com>
2024-07-04 17:51:12 +0200

    Add files via upload

---
+++ test.py
@@ -0,0 +1,33 @@
+company_name= input('please enter the mame of the company: ')
+the_name_of_the_city = input('please enter tne name of the city: ')
+company_address = input('please enter the company address: ')
+post_of_the_company =input('please enter company post: ')
+myname = input('please enter your name: ')
+
+
+
+post_from_practicant =input('please enter your post: ')
+email =input('please enter your email: ')
+place_of_residence = ('please enter your place of residence: ')
+your_website= ('please enter your your website: ')
+# age = input('please enter your age')
+
+
+
+
+print(company_name)
+print(company_address)
+print(post_of_the_company)
+print(myname)
+print(post_from_practicant)
+print()
+print('application for practice')
+print('respected')
+print('I am contacting you to attend an internship in your work environment, which I found on your site: ' + your_website)
+print('I would like to introduce myself and express my interest in cooperation with you')
+print('I have completed secondary catering school, I completed the web designer course at Infoars Academy, that school is located in Banja Luka, but I have online lessons from home, I know German well, I am willing to work and learn, and I want to gain new knowledge. I would be grateful if you could understand my request.')
+print('respectfully yours ' + myname)
+print('best regards')
+
+
+input('please press any key')
